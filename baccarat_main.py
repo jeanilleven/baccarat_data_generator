97246210e8f84336ffe03ad3ae510664006462f9
Jeanille Ven <jeanillevenabayon30@gmail.com>
2021-08-30 18:38:55 +0800

    Fixed overlapping names

--- baccarat_main.py
+++ baccarat_main.py
@@ -7,8 +7,11 @@ from math import *
 import csv
 from random import shuffle, seed
 from faker.providers.person.en import Provider
+import numpy as np
 
-names = [Faker().name() for i in range(3)]
+names = pd.read_csv('uniquenames.csv')['Username'].values
+random.shuffle(names)
+names = names[:10000]
 for name in names:
   player = Player(name)
   player.display_player_details()
@@ -20,9 +23,9 @@ for name in names:
     player.total_chips = table.total_chips
     player.num_of_playthroughs=table.num_of_playthroughs
 
-    # added_seconds = random.choice(range(350, 500))
-    # player.last_active_date = table.list_of_games[-1][0]
-    # player.last_active_date += datetime.timedelta(0, added_seconds)
+    added_seconds = random.choice(range(350, 500))
+    player.last_active_date = table.list_of_games[-1][0]
+    player.last_active_date += datetime.timedelta(0, added_seconds)
     
     player.display_player_details()
     print(f"\nTables Played: {player.num_of_sessions}\nTotal PlayThroughs: {player.num_of_games}\nTotal Games Played: {player.num_of_playthroughs}\n**")
